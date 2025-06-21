40f0c3f1121b97e53edd831e60cceb355e478820
prager <jkulisek.us@gmail.com>
2025-06-21 13:09:26 -0700

    Refreshed dotenv and mongoose installations

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,7 @@ import { getAllUsers, createUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
+router.get("/users", getAllUsers);
 router.post("/NewUser", createUser);
 
 export default router;
